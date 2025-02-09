9386db0fd1e35d3dffe0552f530e50fa8a5ecefb
Mr.Robot <prathameshbaviskar817@gmail.com>
2025-02-09 19:41:56 +0530

    This my setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    "this function will return list of requirements"
+    requirement_lst:list[str]=[]
+    try:
+        with open('requirement.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement !="-e .":
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print('requirement.txt file not found')
+    return requirement_lst
+print(get_requirements())  
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Prathamesh Baviskar",
+    author_email="prathameshbaviskar817@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
